4e1ec639bbd55da99716d5b2f723418d3033bd92
LOyster1 <loyster1@alaska.edu>
2016-10-13 20:55:38 -0800

    Rilke Schulz MVC build

---
+++ Rilke_Schule_Student_Management/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Rilke_Schule_Student_Management
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Rilke_Schule_Student_Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rilke_Schule_Student_Management.Startup))]
+namespace Rilke_Schule_Student_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
