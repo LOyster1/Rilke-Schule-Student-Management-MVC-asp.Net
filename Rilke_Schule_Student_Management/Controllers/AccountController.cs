c6a79c2a5e66f4655d3e9444e7c977b3aff3d0c2
ericpak <pak.eric@gmail.com>
2016-11-18 14:45:57 -0900

    Changed the log in to take the email instead of username

--- Rilke_Schule_Student_Management/Controllers/AccountController.cs
+++ Rilke_Schule_Student_Management/Controllers/AccountController.cs
@@ -70,9 +70,15 @@ namespace Rilke_Schule_Student_Management.Controllers
                 return View(model);
             }
 
+            // Sets user to email
+            var user = await UserManager.FindByEmailAsync(model.Email);
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            // var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+
+            // Sign in using email
+            var result = user == null ? SignInStatus.Failure : await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -148,8 +154,7 @@ namespace Rilke_Schule_Student_Management.Controllers
         {
 
             if (ModelState.IsValid)
-            {//FirstName = model.FirstName, 
-                //var user = new ApplicationUser { UserName = model.UserName, Email = model.Email};
+            {
                 var user = new ApplicationUser
                 {
                     FirstName = model.FirstName,
