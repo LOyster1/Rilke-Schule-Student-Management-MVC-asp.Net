ddd42a8be7cb2c5098a7e4aa86279138c820fa7e
xwakahisax <zerakyo@hotmail.com>
2016-11-18 14:45:49 -0900

    Added Teacher and Class Models

---
+++ Rilke_Schule_Student_Management/Models/Class.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rilke_Schule_Student_Management.Models
+{
+    public class Class
+    {
+        [Key]
+        [Required]
+        public int Class_Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+        [ForeignKey("UserName"), Column(Order = 1)]
+        public virtual Teacher Teacher { get; set; }
+
+        [Required]
+        public int Student_Number { get; set; }
+        [ForeignKey("Student_Number"), Column(Order = 2)]
+        public virtual Student Student { get; set; }
+    }
+}
