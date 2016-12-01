f36a071c8e3411288cb25c08ad79385db34af807
ericpak <pak.eric@gmail.com>
2016-11-30 23:00:55 -0900

    Updating FieldTrip/StudentController

--- Rilke_Schule_Student_Management/Controllers/StudentsController.cs
+++ Rilke_Schule_Student_Management/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Rilke_Schule_Student_Management.Models;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Core.Objects;
 using System.Linq;
@@ -28,35 +29,41 @@ namespace Rilke_Schule_Student_Management.Controllers
         public ActionResult AddStudent(Student viewModel)
         //Get entered Student information and query it against Students, if student exists in Students, add Guardianship entity
         {
-
+            List<int> StudentList = new List<int>();
             var parent = User.Identity.GetUserId();
             
             var F_Name = viewModel.Stud_F_Name;
             var L_Name = viewModel.Stud_L_Name;
             var DOB = viewModel.Date_Of_Birth;
+            bool added = false;
 
 
             var existingStudent = from m in db.Students
-                          where m.Stud_F_Name == F_Name && m.Stud_L_Name==L_Name && DbFunctions.DiffDays(m.Date_Of_Birth, DOB) == 0 
-                        select m;
-            foreach(var t in existingStudent)
+                                  where m.Stud_F_Name == F_Name && m.Stud_L_Name == L_Name
+                                  select m;
+
+            var dateCheck = from m in existingStudent
+                            where DOB.Equals(m.Date_Of_Birth)
+                            select m.Student_Number;
+
+            foreach (int s in dateCheck)
             {
                 Guardianship guardianship = new Guardianship();
                 guardianship.UserName = parent;
-                guardianship.Student_Number = t.Student_Number;
+                guardianship.Student_Number = s;
                 db.Guardianships.Add(guardianship);
-                
+                added = true;
             }
-            db.SaveChanges();
-            if (existingStudent == null)
-            {
 
+            db.SaveChanges();
+            if (!added)
+            {
                 return RedirectToAction("AddStudent", "Students");
             }
             
 
 
-            return View();
+            return RedirectToAction("ManageStudent", "Students");
         }
         [Authorize(Roles = "Parent")]
         public ActionResult ManageStudent()
